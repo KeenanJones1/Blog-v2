fb0686db290b969efd254fd6fd59eecb43d89337
Keenan Jones <keenanjones210@gmail.com>
2020-09-18 09:20:26 -0500

    tag and post tag models and migrations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, :body, :user_id, presence: true
   belongs_to :user
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
+
+  scope :recent, -> { order(created_at: :desc) }
+  scope :latest, -> (number) { recent.limit(number) }
+  scope :published, -> { where.not(published_at: nil) }
+  scope :drafts, -> { where(published_at: nil) }
+  scope :featured, -> { where(featured: true) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   validates :username, presence: true
   validate :avatar_image_size
-  
+
   has_many :posts, dependent: :destroy
 
   
